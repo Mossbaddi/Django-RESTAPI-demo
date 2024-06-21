29e6ae65224d1fd11d15f8a80fe0262661e792b3
Mostapha <mossb.addi@gmail.com>
2024-06-21 23:30:12 +0200

    Configuré CORS

--- EventAPI/settings.py
+++ EventAPI/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'events',
-    'rest_framework'
+    'rest_framework',
+	'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
+	'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+# ]
 
 ROOT_URLCONF = 'EventAPI.urls'
 
