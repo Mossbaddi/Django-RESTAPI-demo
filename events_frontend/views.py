ae67444b5ce4a32ff1ab2d2e2a5ecbb54f5468f2
Mostapha <mossb.addi@gmail.com>
2024-06-22 19:42:35 +0200

    Création du base.html

--- events_frontend/views.py
+++ events_frontend/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from . import views
 def index(request):
     return render(request, 'events_frontend/index.html')
+
+
+def test(request):
+	return render(request, 'events_frontend/test.html')
